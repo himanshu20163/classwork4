c45ef9ad1e857f322aed1710c0aff79472b9d2f7
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-04-25 11:17:29 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Cards_next from './Cards_next';
-import Home_page from './Home/Home_page';
+// import Home_page from './Home/Home_page';
 
 function App() {
   return (
